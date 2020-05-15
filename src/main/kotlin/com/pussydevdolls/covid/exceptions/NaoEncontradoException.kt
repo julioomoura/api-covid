6f05ee75e53002161cf88856b2cba80cef8a08c7
Júlio <juliomourajom@gmail.com>
2020-05-14 21:39:09 -0300

    :cool: Cria exceções personalizadas

---
+++ src/main/kotlin/com/pussydevdolls/covid/exceptions/NaoEncontradoException.kt
@@ -0,0 +1,3 @@
+package com.pussydevdolls.covid.exceptions
+
+class NaoEncontradoException(message: String) : Exception(message)
---
+++ src/main/kotlin/com/pussydevdolls/covid/exceptions/RegistroDeSintomaJaExisteException.kt
@@ -0,0 +1,3 @@
+package com.pussydevdolls.covid.exceptions
+
+class RegistroDeSintomaJaExisteException(message: String) : Exception(message)
