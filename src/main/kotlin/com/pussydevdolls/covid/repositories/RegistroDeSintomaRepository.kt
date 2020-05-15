a03b50f58c3e8a37aeef58f03b57561d7ad45fda
Júlio <juliomourajom@gmail.com>
2020-05-14 21:38:41 -0300

    :sparkles: Adiciona métodos de buscas no banco

--- src/main/kotlin/com/pussydevdolls/covid/repositories/RegistroDeSintomaRepository.kt
+++ src/main/kotlin/com/pussydevdolls/covid/repositories/RegistroDeSintomaRepository.kt
@@ -3,6 +3,11 @@ package com.pussydevdolls.covid.repositories
 import com.pussydevdolls.covid.models.RegistroDeSintoma
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
+import java.util.*
 
 @Repository
-interface RegistroDeSintomaRepository: CrudRepository<RegistroDeSintoma, Long>
+interface RegistroDeSintomaRepository: CrudRepository<RegistroDeSintoma, Long> {
+
+    fun findByUsuarioCpfAndSintomaIdAndData(userCpf: String, sintomaId: Long, data: LocalDate): Optional<RegistroDeSintoma>
+}
--- src/main/kotlin/com/pussydevdolls/covid/repositories/SintomaRepository.kt
+++ src/main/kotlin/com/pussydevdolls/covid/repositories/SintomaRepository.kt
@@ -3,6 +3,9 @@ package com.pussydevdolls.covid.repositories
 import com.pussydevdolls.covid.models.Sintoma
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface SintomaRepository: CrudRepository<Sintoma, Long>
+interface SintomaRepository: CrudRepository<Sintoma, Long> {
+    fun findByNome(nome: String): Optional<Sintoma>
+}
